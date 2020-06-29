96666aa755e6de2e522fd06cc7789317d84f5ba3
Cameron_Murphy <cameronmurphyofficial@gmail.com>
2020-06-29 11:02:13 +1000

    Added skin options for the user

---
+++ ComputerGraphics/Assets/Scripts/ChangeSkin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSkin : MonoBehaviour
+{
+    public Texture redSkin, greenSkin, blueSkin;
+
+    public void ChangeSkinColour(int val)
+    {
+        if(val == 0)
+        {
+            GetComponent<Renderer>().material.SetTexture("_MainTex", redSkin);
+        }
+        if (val == 1)
+        {
+            GetComponent<Renderer>().material.SetTexture("_MainTex", greenSkin);
+        }
+        if (val == 2)
+        {
+            GetComponent<Renderer>().material.SetTexture("_MainTex", blueSkin);
+        }
+    }
+}
